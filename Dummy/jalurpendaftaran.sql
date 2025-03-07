8be481987cda80dbd924311045682ecf01ceb1c3
Marco Marcello Hugo <126767160+Marcellohugo@users.noreply.github.com>
2025-03-07 14:51:20 +0700

    Update jalurpendaftaran.sql

--- Dummy/jalurpendaftaran.sql
+++ Dummy/jalurpendaftaran.sql
@@ -1,9 +1,9 @@
-INSERT INTO `jalurpendaftaran` (`id_jalur`, `nama_jalur`, `jenjang`) VALUES
-(1, 'UTBK', 'SMA'),
-(2, 'Prestasi Akademik', 'SMA'),
-(3, 'Prestasi Non-Akademik', 'SMA'),
-(4, 'Zonasi', 'SMA'),
-(5, 'UTBK', 'SMK'),
-(6, 'Prestasi Akademik', 'SMK'),
-(7, 'Prestasi Non-Akademik', 'SMK'),
-(8, 'Zonasi', 'SMK');
+INSERT INTO `jalurpendaftaran` (`nama_jalur`, `jenjang`) VALUES
+('UTBK', 'SMA'),
+('UTBK', 'SMK'),
+('Prestasi Akademik', 'SMA'),
+('Prestasi Akademik', 'SMK'),
+('Prestasi Non Akademik', 'SMA'),
+('Prestasi Non Akademik', 'SMK'),
+('Zonasi', 'SMA'),
+('Zonasi', 'SMK');
